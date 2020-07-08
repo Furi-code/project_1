b5eb19554fd916ef4cc5aefc5be346cd933c4f3b
Danil <danielman7397@gmail.com>
2020-07-08 15:26:57 +0400

    Task 3

--- js/script.js
+++ js/script.js
@@ -25,23 +25,20 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-
-    },
-    actors: {
-
-    },
-    genres  : [],
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 
 };
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
 
-
-
-
-document.write(answers);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
