750c7c405afea4eeb1731264fbeb89c033d4f7b1
Danil <danielman7397@gmail.com>
2020-07-08 12:42:26 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number= 10;
+// console.log(number);
+
+// var name = 'Ivan';
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj[1]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+
+// const result = confirm('Are you hear?');
+// const answer = [];
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как ваше фамилия?', '');
+// answer[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answer);
+
+const category = 'toys';
+
+console.log(`https://brandem.ru/${category}/page124`);
