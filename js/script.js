d82cd33df38c549b72d39848edc59654515c5ee0
Danil <danielman7397@gmail.com>
2020-07-12 17:51:13 +0400

    Lesson 20 completed

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 'use strict';
+// const obj = new Object();
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors : {
+        border: 'black',
+        bg: 'red'
+    },
+    maketest: function() {
+        console.log('test');
+    }
+};
 
-function second() {
-    console.log(2);
-}
-first();
-second();
+options.maketest();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-function done() {
-    console.log(`Я прошёл этот урок!`);
-}
-learnJS('JavaScript', done);
+const {border, bg} = options.colors; 
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object'){
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         counter++;
+//         }
+//     } else {
+
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//     }
+//     counter++;
+// }
+// console.log (counter);
